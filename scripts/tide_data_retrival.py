7e36af0723e685e80bdbebc6e1b5811d166ca452
heatherbaier <zhonghui.lv@gmail.com>
2023-10-16 20:16:58 +0000

    sync ts4, add code to get stations' coordinates

--- scripts/tide_data_retrival.py
+++ scripts/tide_data_retrival.py
@@ -11,56 +11,42 @@ from typing import Optional, Union
 import pandas as pd
 import requests
 import geopandas as gpd
+from shapely.geometry import Point
 
-# referenced from https://github.com/GClunies/noaa_coops/blob/master/noaa_coops/station.py
 
-def get_stations_from_bbox(
-    lat_coords: list[float, float],
-    lon_coords: list[float, float],
-) -> list[str]:
+def get_stations_from_bbox(geofile):
+    
     """Return a list of stations IDs found within a bounding box.
-
-    Args:
-        lat_coords (list[float]): The lower and upper latitudes of the box.
-        lon_coords (list[float]): The lower and upper longitudes of the box.
-
-    Raises:
-        ValueError: lat_coords or lon_coords are not of length 2.
-
     Returns:
-        list[str]: A list of station IDs.
+        GeoDataFrame[str]: A geodataframe with all stations' coordinates, id, and name
     """
-    station_list = []
+    
     data_url = "https://api.tidesandcurrents.noaa.gov/mdapi/prod/webapi/stations.json"
     response = requests.get(data_url)
     json_dict = response.json()
-
-    if len(lat_coords) != 2 or len(lon_coords) != 2:
-        raise ValueError("lat_coords and lon_coords must be of length 2.")
-
-    # Ensure lat_coords and lon_coords are in the correct order
-    lat_coords = sorted(lat_coords)
-    lon_coords = sorted(lon_coords)
-
-    # if lat_coords[0] > lat_coords[1]:
-    #     lat_coords[0], lat_coords[1] = lat_coords[1], lat_coords[0]
-
-    # if lon_coords[0] > lon_coords[1]:
-    #     lon_coords[0], lon_coords[1] = lon_coords[1], lon_coords[0]
-
-    # Find stations in bounding box
-    for station_dict in json_dict["stations"]:
-        if lon_coords[0] < station_dict["lng"] < lon_coords[1]:
-            if lat_coords[0] < station_dict["lat"] < lat_coords[1]:
-                station_list.append(station_dict["id"])
-
-    return station_list
+    
+    stations = []
+    for station in json_dict["stations"]:
+        station_info = []
+        station_info.append(station['name'])
+        station_info.append(station['id'])
+        station_info.append(station['lat'])
+        station_info.append(station['lng'])
+        
+        stations.append(station_info)
+    
+    df = pd.DataFrame(stations, columns=['name', 'id', 'lat', 'lng'])
+    geometry = [Point(xy) for xy in zip(df.lng, df.lat)]
+    points_gdf = gpd.GeoDataFrame(df, crs="EPSG:4326", geometry=geometry)
+    
+    bound_gdf = gpd.read_file(geofile, driver='GeoJSON')
+    sub_gdf = points_gdf[points_gdf.within(bound_gdf.loc[0, 'geometry'])]
+    
+    return sub_gdf
 
 
 
 
-
-station_id = '8638610'
 begin_date = ''
 end_date = ''
 product_name = ''
@@ -68,12 +54,12 @@ datum = 'NAVD'
 time_zone = ''
 units = 'metric'
 
-root_path = os.path.abspath('..')
 
-print(root_path)
-# polygons_file = os.path.join()
-#
-# gpd.read_file()
+root_path = os.path.abspath('..')
+bounding_file = os.path.join(root_path, 'data/boundary.geojson')
+
+stations_df = get_stations_from_bbox(bounding_file)
+
 
 
 
